c3975bab81911022e33b1b724963a6b505bb948c
Jeff Vazquez <jeffvazquez@cpp.edu>
2023-09-12 08:33:22 -0700

    Fixing the saving configuration feature

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -1,11 +1,12 @@
 package com.bignerdranch.android.geoquiz
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
-
-class QuizViewModel: ViewModel() {
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+class QuizViewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
     private val questionBank = listOf(
         Question(R.string.question_Australia,true),
         Question(R.string.question_California, true),
@@ -13,17 +14,26 @@ class QuizViewModel: ViewModel() {
         Question(R.string.question_USA,true)
     )
 
-    private var currentIndex = 0
+    private var  currentIndex: Int
+        get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY,value)
+
 
     val currentQuestionAnswer:Boolean
         get() = questionBank[currentIndex].answer
 
     val currentQuestionText:Int
         get() = questionBank[currentIndex].textResId
+
     fun moveToNext(){
         currentIndex = (currentIndex + 1) % questionBank.size
     }
     fun moveToPrev(){
-        currentIndex = (currentIndex - 1) % questionBank.size
+        if(currentIndex != 0){
+           currentIndex -= 1
+        }
+        else{
+            currentIndex = questionBank.size - 1
+        }
     }
 }
